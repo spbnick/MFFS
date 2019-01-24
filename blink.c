4adc4a36d4481eca70d0b8046b08901f1fa3390c
Nikolai Kondrashov <spbnick@gmail.com>
2019-01-24 15:14:42 +0200

    Fix RCC register name

--- blink.c
+++ blink.c
@@ -1,12 +1,12 @@
 void
 reset(void)
 {
-    volatile unsigned int *rcc_apb1enr = (unsigned int *)(0x40021000 + 0x18);
+    volatile unsigned int *rcc_apb2enr = (unsigned int *)(0x40021000 + 0x18);
     volatile unsigned int *gpioc_crh = (unsigned int *)(0x40011000 + 0x04);
     volatile unsigned int *gpioc_odr = (unsigned int *)(0x40011000 + 0x0c);
     volatile unsigned int i;
     /* Enable clock to GPIO port C */
-    *rcc_apb1enr |= 1 << 4;
+    *rcc_apb2enr |= 1 << 4;
     /* Configure the PC13 pin for open-drain output, 2MHz max speed */
     *gpioc_crh = (*gpioc_crh & ~ (0xf << ((13-8)*4))) |
                  (6 << ((13-8)*4));
